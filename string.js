e89b14b6ce7a6956915a5e5309394165a22bd460
Aditya Rajaram Gaikar <gaikaditya795@gmail.com>
2026-01-20 16:16:03 +0530

    Grasped the data type String

---
+++ string.js
@@ -0,0 +1,18 @@
+// var str:string = "Aditya Gaikar"
+// var str2:string = 'Hello How are you';
+// var str3:string = `Hello How are you`;
+// var age:number = 30;
+// var userName:string = "Aditya Gaikar";
+// var info:string = `my name is ${userName} and age is ${age}`;
+// console.log(info);
+var num = 100;
+// var data1:string = num.toString();
+// var data1:string = " "+ num;
+var boolData = true;
+// var data1:string = boolData.toString();
+var data1 = "" + boolData;
+var item = true;
+var other;
+other = item;
+console.log(other);
+var info1 = 'aditya';
