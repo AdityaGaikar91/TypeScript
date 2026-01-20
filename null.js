3672ac677b64a4e1ba5e39f70d18169d0f3569e4
Aditya Rajaram Gaikar <gaikaditya795@gmail.com>
2026-01-20 16:37:07 +0530

    Completed Null

---
+++ null.js
@@ -0,0 +1,8 @@
+var data = null;
+data = "Aditya";
+var userName = null;
+var login = true;
+if (login) {
+    userName = "Aditya gaikar";
+}
+console.log(typeof data);
---
+++ null.ts
@@ -0,0 +1,13 @@
+var data: null | string = null;
+data = "Aditya"
+var userName: null | string = null;
+var login = true
+
+
+if(login){
+    userName= "Aditya gaikar"
+}
+
+console.log(typeof data);
+
+
